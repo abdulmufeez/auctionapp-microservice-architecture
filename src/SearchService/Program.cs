06199afbf80f2461c28ffee724f39aab51325cb1
Abdul Mufeez <abdul.mufeez@tpsonline.com>
2023-11-25 21:56:20 +0500

    Making service resilient by adding policy to httpclient

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,10 +1,14 @@
+using System.Net;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService;
 using SearchService.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+//we can also applied policy to any services which is being registered by using AddPolicyHandler()
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -12,13 +16,27 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// by doing this first app is started then code block runs which try catch
+// so even policy of httpclient runs forerver app has started before earlier
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+
+});
 
 app.Run();
+
+// created a policy which catches errors which created not by 
+// intentionally and also catch 404 error and try forerver with 3 secs gape
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(response => response.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
